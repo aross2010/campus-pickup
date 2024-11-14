6fae5edf8914ea6087ec486c55cf2ba09f8a85fd
Alex Ross <adross1027@gmail.com>
2024-11-13 19:29:15 -0800

    seed db with schools and users

---
+++ server/testing/seed.ts
@@ -0,0 +1,90 @@
+import client from '../libs/prisma'
+import { School, User } from '@prisma/client'
+import {
+  bios,
+  firstNames,
+  lastNames,
+  majors,
+  schools,
+  schoolYear,
+  sports,
+} from '../libs/data'
+
+/* This page seeds the database with random data for use in testing. */
+
+const seedUsers = async (numUsers: number) => {
+  const users: User[] = []
+
+  await client.user.deleteMany()
+
+  const schools = (await client.school.findMany()) as School[]
+
+  for (let i = 0; i < numUsers; i++) {
+    const school = schools[Math.floor(Math.random() * schools.length)]
+    const schoolId = school.id
+    const firstName = firstNames[Math.floor(Math.random() * firstNames.length)]
+    const lastName = lastNames[Math.floor(Math.random() * lastNames.length)]
+    const email = `${firstName}.${lastName}${Math.random() * 100}@${
+      school.emailDomain
+    }`
+    const password = 'password'
+    const bio = bios[Math.floor(Math.random() * bios.length)]
+    const profileImage = 'https://picsum.photos/200'
+    const coverImage = 'https://picsum.photos/800/200'
+    const major = majors[Math.floor(Math.random() * majors.length)]
+    // random number between 1 and 4
+    const numSports = Math.floor(Math.random() * 4) + 1
+    const favoriteSports = [] as string[]
+    for (let j = 0; j < numSports; j++) {
+      // random n amount of sports - no duplicates
+      const sport = sports[Math.floor(Math.random() * sports.length)]
+      if (!favoriteSports.includes(sport)) {
+        favoriteSports.push(sport)
+      }
+    }
+    const year = schoolYear[Math.floor(Math.random() * schoolYear.length)]
+
+    try {
+      const [user, updatedSchool] = await client.$transaction([
+        client.user.create({
+          data: {
+            schoolId,
+            firstName,
+            lastName,
+            email,
+            password,
+            bio,
+            profileImage,
+            coverImage,
+            major,
+            favoriteSports,
+            schoolYear: year,
+          },
+        }),
+        client.school.update({
+          where: { id: schoolId },
+          data: {
+            numUsers: {
+              increment: 1,
+            },
+          },
+        }),
+      ])
+    } catch (e) {
+      console.error(e)
+    }
+  }
+}
+
+const seedSchools = async () => {
+  await client.school.deleteMany()
+
+  schools.forEach(async (school) => {
+    const uni = await client.school.create({
+      data: school,
+    })
+  })
+}
+
+// seedSchools()
+// seedUsers(100)
