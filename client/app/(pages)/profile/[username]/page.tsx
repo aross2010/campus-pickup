597d834a312e0cac651b7293f0c22026a983b1e9
Phuong Tong <yenphuongtong97@gmail.com>
2024-11-29 19:58:54 -0800

    add margin

--- client/app/(pages)/profile/[username]/page.tsx
+++ client/app/(pages)/profile/[username]/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function UserProfile() {
     return (
-        <div className="w-full min-h-screen">
+        <div className="w-full min-h-screen mb-20">
             <div className="w-full h-[450px] bg-[#e5a823] relative">
                 <div className="absolute top-4 left-4 flex flex-col items-center">
                     <img
