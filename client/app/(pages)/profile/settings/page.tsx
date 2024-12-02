546905c8fb7de25ddce8637bc87422f0c046e35c
Phuong Tong <yenphuongtong97@gmail.com>
2024-12-01 22:29:23 -0800

    initialize user setting form

---
+++ client/app/(pages)/profile/settings/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function UserSettings() {
+  return (
+    <div className="w-full min-h-screen mb-20">
+        <h1 className = "text-white">
+        Hi
+    </h1>
+
+    </div>
+  )
+}
